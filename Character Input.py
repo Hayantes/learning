3f750c0b2ed92cb13da241fa85cb512355ddb09f
Hayantes <bekzhanissakbayev@gmail.com>
2025-02-20 14:47:08 +0500

    Add files via upload
    damn daniel

---
+++ Character Input.py
@@ -0,0 +1,8 @@
+print("Hello, My name is Bob ! What is your name ?")
+name = input("My name is: ")
+print("You're welcome "+ name)
+print("I'm 21 years old, what about you ?")
+age = int(input("My age is : "))
+one_hundred_years_after_age = 100 + age
+print(f"Wow, so you're gonna be like " + str(one_hundred_years_after_age) + " years old after 100 years ?")
+print("I will hit my 100 years old mark in 79 years and you will hit it in " + str(100 - age) + " years")
